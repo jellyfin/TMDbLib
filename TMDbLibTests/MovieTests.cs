8e056ca893a529317d5699af2b9b992778ec2dbd
Michael Bisbjerg <michael@mbwarez.dk>
2013-02-13 00:37:29 +0100

    - Some renaming

--- TMDbLibTests/MovieTests.cs
+++ TMDbLibTests/MovieTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TMDbLib.Objects.Companies;
 using TMDbLib.Objects.Movies;
 using System.Linq;
 
