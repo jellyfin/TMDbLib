de0993f6db692562f4a04bc04fcc7608ed1ec7b4
Michael Bisbjerg <michael@mbwarez.dk>
2013-02-16 20:35:33 +0100

    - Set the version to 0.7 and created a Nuget package

--- TMDbLib/Properties/AssemblyInfo.cs
+++ TMDbLib/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TMDbLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for the Movie Database TMDb")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TMDbLib")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
