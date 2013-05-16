1867fc6f93c44c44ce9bb56d9d43b2d854aa03db
Michael Bisbjerg <michael@mbwarez.dk>
2013-05-16 20:42:13 +0200

    - Fixed issue #10
    - Changed the model slightly, but should be backwards compatible

--- TMDbLib/Objects/Search/SearchMovie.cs
+++ TMDbLib/Objects/Search/SearchMovie.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 namespace TMDbLib.Objects.Search
 {
@@ -8,11 +9,22 @@ namespace TMDbLib.Objects.Search
         public bool Adult { get; set; }
         public string BackdropPath { get; set; }
         public string OriginalTitle { get; set; }
-        public DateTime ReleaseDate { get; set; }
+        public string Release_Date { get; set; }
         public string PosterPath { get; set; }
         public double Popularity { get; set; }
         public string Title { get; set; }
         public double VoteAverage { get; set; }
         public int VoteCount { get; set; }
+
+        public DateTime ReleaseDate
+        {
+            get
+            {
+                DateTime dt;
+                DateTime.TryParseExact(Release_Date, "yyyy-MM-dd", CultureInfo.CurrentCulture, DateTimeStyles.None, out dt);
+
+                return dt;
+            }
+        }
     }
 }
