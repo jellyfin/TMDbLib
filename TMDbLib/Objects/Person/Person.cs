2ecfee93b0a7bed5ea4c1814c53bae4ed3bb8be5
Naliath <thomas.vriens@outlook.com>
2013-09-29 15:03:45 +0200

    Added date types, changed over to standard restsharp after they made the required changes, minor test changes

--- TMDbLib/Objects/Person/Person.cs
+++ TMDbLib/Objects/Person/Person.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using TMDbLib.Objects.Movies;
 
 namespace TMDbLib.Objects.Person
@@ -6,10 +7,10 @@ namespace TMDbLib.Objects.Person
     public class Person
     {
         public bool Adult { get; set; }
-        public List<string> AlsoKnownAs { get; set; }       // Todo: Find actor with an alternate name to test this
+        public List<string> AlsoKnownAs { get; set; }
         public string Biography { get; set; }
-        public string Birthday { get; set; }        // In this context, dates come as empty strings when unavailable. This can't be parsed.
-        public string Deathday { get; set; }        // In this context, dates come as empty strings when unavailable. This can't be parsed.
+		public DateTime? Birthday { get; set; }
+		public DateTime? Deathday { get; set; }
         public string Homepage { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
