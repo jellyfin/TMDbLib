dd27e838d9f80df2715fea44183fda78708d5d4b
Naliath <thomas.vriens@outlook.com>
2013-11-08 17:26:46 +0100

    Added missing test file

---
+++ TMDbLibTests/ClientNetworkTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TMDbLib.Objects.TvShows;
+
+namespace TMDbLibTests
+{
+    [TestClass]
+    public class ClientNetworkTests
+    {
+        private const int Hbo = 49;
+        private TestConfig _config;
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            _config = new TestConfig();
+        }
+
+        [TestMethod]
+        public void TestNetworkGetById()
+        {
+            Network network = _config.Client.GetNetwork(Hbo);
+
+            Assert.IsNotNull(network);
+            Assert.AreEqual("HBO", network.Name);
+            Assert.AreEqual(Hbo, network.Id);
+        }
+    }
+}
