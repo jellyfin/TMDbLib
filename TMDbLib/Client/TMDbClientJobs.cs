e1507b1f8898a406742473dea3c30d8aaab9fd7b
Steve <stevethoms@gmail.com>
2014-12-10 19:46:46 -0600

    Added jobs namespace (/job/list)
    No additional parameters exposed on the underlying API, so this simply
    exposes a method to return all departments and jobs within

---
+++ TMDbLib/Client/TMDbClientJobs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RestSharp;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Jobs;
+
+namespace TMDbLib.Client
+{
+    public partial class TMDbClient
+    {
+        /// <summary>
+        /// Retrieves a list of departments and positions within
+        /// </summary>
+        /// <returns>Valid jobs and their departments</returns>
+        public List<Job> GetJobs()
+        {
+            RestRequest req = new RestRequest("job/list");
+
+            IRestResponse<JobContainer> response = _client.Get<JobContainer>(req);
+
+            if (response == null || response.Data == null)
+            {
+                return null;
+            }
+
+            return response.Data.Jobs;
+        }
+    }
+}
