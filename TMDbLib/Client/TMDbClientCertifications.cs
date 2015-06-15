0e7eaa1866b5c20f7a3a11f86895ce582c1c3342
Michael Bisbjerg <michael@mbwarez.dk>
2015-06-15 19:45:56 +0200

    Added Certifications implementation
    Issue #90

---
+++ TMDbLib/Client/TMDbClientCertifications.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using TMDbLib.Objects.Certifications;
+
+namespace TMDbLib.Client
+{
+    public partial class TMDbClient
+    {
+        public CertificationsContainer GetMovieCertifications()
+        {
+            RestRequest req = new RestRequest("certification/movie/list");
+
+            IRestResponse<CertificationsContainer> resp = _client.Get<CertificationsContainer>(req);
+
+            return resp.Data;
+        }
+
+        public CertificationsContainer GetTvCertifications()
+        {
+            RestRequest req = new RestRequest("certification/tv/list");
+
+            IRestResponse<CertificationsContainer> resp = _client.Get<CertificationsContainer>(req);
+
+            return resp.Data;
+        }
+    }
+}
