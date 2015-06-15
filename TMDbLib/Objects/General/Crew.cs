dea439ca8fc174c4317f6d9a553fb169d3b5fa0f
Michael Bisbjerg <michael@mbwarez.dk>
2015-06-16 01:07:04 +0200

    Added missing fields

--- TMDbLib/Objects/General/Crew.cs
+++ TMDbLib/Objects/General/Crew.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Department { get; set; }
+        public string CreditId { get; set; }
         public string Job { get; set; }
         public string ProfilePath { get; set; }
     }
