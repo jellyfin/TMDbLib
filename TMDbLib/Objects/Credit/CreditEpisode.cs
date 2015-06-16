213a7ea528d7006fc424f8613d2cf6c4a2cc65f9
Michael Bisbjerg <michael@mbwarez.dk>
2015-06-16 21:21:42 +0200

    Add Credit implementation

---
+++ TMDbLib/Objects/Credit/CreditEpisode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TMDbLib.Objects.Credit
+{
+    public class CreditEpisode
+    {
+        public DateTime? AirDate { get; set; }
+        public int EpisodeNumber { get; set; }
+        public string Name { get; set; }
+        public string Overview { get; set; }
+        public int SeasonNumber { get; set; }
+        public string StillPath { get; set; }
+    }
+}
