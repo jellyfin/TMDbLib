23da40339de912228f31f741213e91d8ddc3e67d
Michael Bisbjerg <michael@mbwarez.dk>
2015-06-18 22:56:45 +0200

    Add initial Guest sessions code

---
+++ TMDbLib/Client/TMDbClientGuestSessions.cs
@@ -0,0 +1,39 @@
+﻿using RestSharp;
+using TMDbLib.Objects.Authentication;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Movies;
+
+namespace TMDbLib.Client
+{
+    public partial class TMDbClient
+    {
+        public SearchContainer<Movie> GetGuestSessionRatedMovies(int page = 0)
+        {
+            return GetGuestSessionRatedMovies(DefaultLanguage, page);
+        }
+
+        public SearchContainer<Movie> GetGuestSessionRatedMovies(string language, int page = 0)
+        {
+            RequireSessionId(SessionType.GuestSession);
+
+            RestRequest request = new RestRequest("guest_session/{guest_session_id}/rated_movies") { RequestFormat = DataFormat.Json };
+
+            if (page > 0)
+                request.AddParameter("page", page, ParameterType.QueryString);
+
+            if (!string.IsNullOrEmpty(language))
+                request.AddParameter("language", language, ParameterType.QueryString);
+
+            // TODO: Can we use user session for this?
+            //if (SessionType == SessionType.UserSession)
+            //    request.AddParameter("session_id", SessionId, ParameterType.QueryString);
+            //else
+            request.AddUrlSegment("guest_session_id", SessionId);
+
+
+            IRestResponse<SearchContainer<Movie>> resp = _client.Post<SearchContainer<Movie>>(request);
+
+            return resp.Data;
+        }
+    }
+}
---
+++ TMDbLibTests/ClientGuestSessionTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TMDbLib.Objects.Authentication;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Movies;
+using TMDbLibTests.Helpers;
+
+namespace TMDbLibTests
+{
+    [TestClass]
+    public class ClientGuestSessionTests
+    {
+        private TestConfig _config;
+
+        /// <summary>
+        /// Run once, on every test
+        /// </summary>
+        [TestInitialize]
+        public void Initiator()
+        {
+            _config = new TestConfig();
+        }
+
+        [TestMethod]
+        public void TestGuestSessionGetRatedMovies()
+        {
+            _config.Client.SetSessionInformation(_config.GuestTestSessionId, SessionType.GuestSession);
+
+            // Test paging
+            TestHelpers.SearchPages(i => _config.Client.GetGuestSessionRatedMovies(i));
+
+            // Fetch ratings
+            SearchContainer<Movie> result = _config.Client.GetGuestSessionRatedMovies();
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Results);
+        }
+    }
+}
