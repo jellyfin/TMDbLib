ca6189d450f35346533eebf76369913cdc270791
Michael Bisbjerg <michael@mbwarez.dk>
2015-06-19 01:11:10 +0200

    Add missing sorts and rewrite names

--- TMDbLib/Objects/Discover/DiscoverTvShowSortBy.cs
+++ TMDbLib/Objects/Discover/DiscoverTvShowSortBy.cs
@@ -4,18 +4,17 @@ namespace TMDbLib.Objects.Discover
 {
     public enum DiscoverTvShowSortBy
     {
-        Undefined = 0,
-        [Description("vote_average.desc")]
-        VoteAverageDescending = 1,
         [Description("vote_average.asc")]
-        VoteAverageAscending = 2,
-        [Description("first_air_date.desc")]
-        FirstAirDateDescending = 3,
+        VoteAverage,
+        [Description("vote_average.desc")]
+        VoteAverageDesc,
         [Description("first_air_date.asc")]
-        FirstAirDateAscending = 4,
-        [Description("popularity.desc")]
-        PopularityDescending = 5,
+        FirstAirDate,
+        [Description("first_air_date.desc")]
+        FirstAirDateDesc,
         [Description("popularity.asc")]
-        PopularityAscending = 6
+        Popularity,
+        [Description("popularity.desc")]
+        PopularityDesc
     }
 }
