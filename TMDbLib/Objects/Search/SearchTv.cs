b5ac0dd8d1a17eda6a7d1212f36667abeef98b65
Michael Bisbjerg <michael@mbwarez.dk>
2015-07-28 22:59:12 +0200

    Added new fiels to SearchTv

--- TMDbLib/Objects/Search/SearchTv.cs
+++ TMDbLib/Objects/Search/SearchTv.cs
@@ -8,12 +8,15 @@ namespace TMDbLib.Objects.Search
         public int Id { get; set; }
         public string BackdropPath { get; set; }
         public string OriginalName { get; set; }
+        public string OriginalLanguage { get; set; }
         public DateTime? FirstAirDate { get; set; }
         public string PosterPath { get; set; }
         public double Popularity { get; set; }
         public string Name { get; set; }
+        public string Overview { get; set; }
         public double VoteAverage { get; set; }
         public int VoteCount { get; set; }
         public List<string> OriginCountry { get; set; }
+        public List<int> GenreIds { get; set; }
     }
 }
