fab03dce69813b2f9afd2fe8ecc7e74157f0e1d5
Michael Bisbjerg <michael@mbwarez.dk>
2015-11-26 00:35:37 +0100

    Converted Genres

--- TMDbLib/Client/TMDbClientGenres.cs
+++ TMDbLib/Client/TMDbClientGenres.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-﻿using System;
-﻿using RestSharp;
+using System;
 using TMDbLib.Objects.General;
 using TMDbLib.Objects.Genres;
+using TMDbLib.Utilities;
 
 namespace TMDbLib.Client
 {
@@ -16,18 +16,15 @@ namespace TMDbLib.Client
 
         public async Task<List<Genre>> GetMovieGenres(string language)
         {
-            RestRequest req = new RestRequest("genre/movie/list");
+            TmdbRestRequest req = _client2.Create("genre/movie/list");
 
             language = language ?? DefaultLanguage;
             if (!String.IsNullOrWhiteSpace(language))
                 req.AddParameter("language", language);
 
-            IRestResponse<GenreContainer> resp = await _client.ExecuteGetTaskAsync<GenreContainer>(req);
+            TmdbRestResponse<GenreContainer> resp = await req.ExecuteGetTaskAsync<GenreContainer>();
 
-            if (resp.Data == null)
-                return null;
-
-            return resp.Data.Genres;
+            return (await resp.GetDataObject()).Genres;
         }
 
         public async Task<List<Genre>> GetTvGenres()
@@ -37,18 +34,15 @@ namespace TMDbLib.Client
 
         public async Task<List<Genre>> GetTvGenres(string language)
         {
-            RestRequest req = new RestRequest("genre/tv/list");
+            TmdbRestRequest req = _client2.Create("genre/tv/list");
 
             language = language ?? DefaultLanguage;
             if (!String.IsNullOrWhiteSpace(language))
                 req.AddParameter("language", language);
 
-            IRestResponse<GenreContainer> resp = await _client.ExecuteGetTaskAsync<GenreContainer>(req);
+            TmdbRestResponse<GenreContainer> resp = await req.ExecuteGetTaskAsync<GenreContainer>();
 
-            if (resp.Data == null)
-                return null;
-
-            return resp.Data.Genres;
+            return (await resp.GetDataObject()).Genres;
         }
 
         public async Task<SearchContainerWithId<MovieResult>> GetGenreMovies(int genreId, int page = 0, bool? includeAllMovies = null)
@@ -58,7 +52,7 @@ namespace TMDbLib.Client
 
         public async Task<SearchContainerWithId<MovieResult>> GetGenreMovies(int genreId, string language, int page = 0, bool? includeAllMovies = null)
         {
-            RestRequest req = new RestRequest("genre/{genreId}/movies");
+            TmdbRestRequest req = _client2.Create("genre/{genreId}/movies");
             req.AddUrlSegment("genreId", genreId.ToString());
 
             language = language ?? DefaultLanguage;
@@ -66,13 +60,13 @@ namespace TMDbLib.Client
                 req.AddParameter("language", language);
 
             if (page >= 1)
-                req.AddParameter("page", page);
+                req.AddParameter("page", page.ToString());
             if (includeAllMovies.HasValue)
                 req.AddParameter("include_all_movies", includeAllMovies.Value ? "true" : "false");
 
-            IRestResponse<SearchContainerWithId<MovieResult>> resp = await _client.ExecuteGetTaskAsync<SearchContainerWithId<MovieResult>>(req).ConfigureAwait(false);
+            TmdbRestResponse<SearchContainerWithId<MovieResult>> resp = await req.ExecuteGetTaskAsync<SearchContainerWithId<MovieResult>>().ConfigureAwait(false);
 
-            return resp.Data;
+            return resp;
         }
     }
 }
