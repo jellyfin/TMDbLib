a6d395828ac5878951185f24af703de92f97b26d
Michael Bisbjerg <michael@mbwarez.dk>
2016-01-27 19:44:40 +0100

    Add new dictionary-like collection
    #173

--- TMDbLib/Client/TMDbClientDiscover.cs
+++ TMDbLib/Client/TMDbClientDiscover.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Specialized;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using TMDbLib.Objects.Discover;
 using TMDbLib.Objects.General;
 using TMDbLib.Rest;
+using TMDbLib.Utilities;
 
 namespace TMDbLib.Client
 {
@@ -24,7 +25,7 @@ namespace TMDbLib.Client
             return new DiscoverMovie(this);
         }
 
-        internal async Task<SearchContainer<T>> DiscoverPerform<T>(string endpoint, string language, int page, NameValueCollection parameters)
+        internal async Task<SearchContainer<T>> DiscoverPerform<T>(string endpoint, string language, int page, SimpleNamedValueCollection parameters)
         {
             RestRequest request = _client.Create(endpoint);
 
@@ -34,8 +35,8 @@ namespace TMDbLib.Client
             if (!string.IsNullOrWhiteSpace(language))
                 request.AddParameter("language", language);
 
-            foreach (string key in parameters.Keys)
-                request.AddParameter(key, parameters[key]);
+            foreach (KeyValuePair<string, string> pair in parameters)
+                request.AddParameter(pair.Key, pair.Value);
 
             RestResponse<SearchContainer<T>> response = await request.ExecuteGet<SearchContainer<T>>().ConfigureAwait(false);
             return response;
