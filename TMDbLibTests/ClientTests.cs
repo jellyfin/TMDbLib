64f9765215765a0d02323ba5241bd381602abc6a
Michael Bisbjerg <michael@mbwarez.dk>
2016-07-20 23:03:28 +0200

    Skip JSON checking for load tests

--- TMDbLibTests/ClientTests.cs
+++ TMDbLibTests/ClientTests.cs
@@ -83,6 +83,9 @@ namespace TMDbLibTests
         [Fact]
         public void ClientRateLimitTest()
         {
+            IgnoreMissingJson = true;
+            IgnoreMissingProperties = true;
+
             const int id = IdHelper.AGoodDayToDieHard;
 
             TMDbClient client = new TMDbClient(TestConfig.APIKey);
