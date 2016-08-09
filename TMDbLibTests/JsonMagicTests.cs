793dcea59deb2a725fc9f29bad59a8f2865957ca
Michael Bisbjerg <michael@mbwarez.dk>
2016-08-09 22:23:01 +0200

    Add test for new ChangeItemConverter

--- TMDbLibTests/JsonMagicTests.cs
+++ TMDbLibTests/JsonMagicTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using TMDbLib.Objects.Authentication;
+using TMDbLib.Objects.Changes;
 using TMDbLib.Objects.General;
 using TMDbLib.Objects.Movies;
 using TMDbLib.Objects.People;
@@ -120,6 +121,34 @@ namespace TMDbLibTests
             Assert.Equal(IdHelper.BigBangTheorySeason1Episode2Id, episode.Id);
             Assert.Equal(2, episode.EpisodeNumber);
             Assert.Null(episode.Rating);
+        }
+
+        /// <summary>
+        /// Tests the ChangeItemConverter
+        /// </summary>
+        [Fact]
+        public void TestChangeItemConverter()
+        {
+            // Ignore missing movie properties
+            IgnoreMissingJson(" / account_states", " / alternative_titles", " / changes", " / credits", " / images", " / keywords", " / lists", " / release_dates", " / releases", " / reviews", " / similar", " / translations", " / videos");
+
+            Movie latestMovie = Config.Client.GetMovieLatestAsync().Sync();
+            List<Change> changes = Config.Client.GetMovieChangesAsync(latestMovie.Id).Sync();
+            List<ChangeItemBase> changeItems = changes.SelectMany(s => s.Items).ToList();
+
+            ChangeAction[] actions = { ChangeAction.Added, ChangeAction.Created, ChangeAction.Updated };
+
+            Assert.NotEmpty(changeItems);
+            Assert.All(changeItems, item => Assert.Contains(item.Action, actions));
+
+            IEnumerable<ChangeItemBase> items = changeItems.Where(s => s.Action == ChangeAction.Added);
+            Assert.All(items, item => Assert.IsType<ChangeItemAdded>(item));
+
+            items = changeItems.Where(s => s.Action == ChangeAction.Updated);
+            Assert.All(items, item => Assert.IsType<ChangeItemUpdated>(item));
+
+            items = changeItems.Where(s => s.Action == ChangeAction.Created);
+            Assert.All(items, item => Assert.IsType<ChangeItemCreated>(item));
         }
     }
 }
