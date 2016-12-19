1e57c679e648134f16bf1ef9d5efcc7539d3fbc3
Michael Bisbjerg <michael@mbwarez.dk>
2016-12-20 00:06:57 +0100

    Make tests of converters possible

--- TMDbLib/Properties/AssemblyInfo.cs
+++ TMDbLib/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -13,3 +14,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c8cd805d-f17a-4919-9adb-b5f50d72f32a")]
+
+[assembly: InternalsVisibleTo("TMDbLibTests")]
