412d207e1bd82d5ff357fb452808481999ac636a
Lukas Rögner <lukas.roegner@icloud.com>
2018-11-16 12:12:31 +0100

    Added the origin country to the company.

--- TMDbLibTests/ClientCompanyTests.cs
+++ TMDbLibTests/ClientCompanyTests.cs
@@ -133,6 +133,7 @@ namespace TMDbLibTests
             Assert.Equal("Columbia Pictures Industries, Inc. (CPII) is an American film production and distribution company. Columbia Pictures now forms part of the Columbia TriStar Motion Picture Group, owned by Sony Pictures Entertainment, a subsidiary of the Japanese conglomerate Sony. It is one of the leading film companies in the world, a member of the so-called Big Six. It was one of the so-called Little Three among the eight major film studios of Hollywood's Golden Age.", company.Description);
             Assert.Equal("Culver City, California", company.Headquarters);
             Assert.Equal("http://www.sonypictures.com/", company.Homepage);
+            Assert.Equal("US", company.OriginCountry);
             Assert.Equal("/mjUSfXXUhMiLAA1Zq1TfStNSoLR.png", company.LogoPath);
             Assert.Equal("Columbia Pictures", company.Name);
 
