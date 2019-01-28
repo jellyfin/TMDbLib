1f8cc6936261c431935f0dda8010ed92f07d6b5e
Frederik Bolding <frederik.bolding@gmail.com>
2019-01-28 13:39:20 +0100

    Added tv episode groups

---
+++ TMDbLib/Objects/TvShows/TvGroupNetwork.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace TMDbLib.Objects.TvShows
+{
+    public class TvGroupNetwork : NetworkBase
+    {
+        [JsonProperty("logo_path")]
+        public string LogoPath { get; set; }
+
+        [JsonProperty("origin_country")]
+        public string OriginCountry { get; set; }
+    }
+}
