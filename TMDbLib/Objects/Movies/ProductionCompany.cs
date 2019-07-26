71f2e001015fd53c4ea01f76ea38a530cff2136c
Frederik Bolding <frederik.bolding@gmail.com>
2019-07-26 18:44:07 +0200

    Added new fields to ProductionCompany

--- TMDbLib/Objects/Movies/ProductionCompany.cs
+++ TMDbLib/Objects/Movies/ProductionCompany.cs
@@ -9,5 +9,11 @@ namespace TMDbLib.Objects.Movies
 
         [JsonProperty("name")]
         public string Name { get; set; }
+
+        [JsonProperty("logo_path")]
+        public string LogoPath { get; set; }
+
+        [JsonProperty("origin_country")]
+        public string OriginCountry { get; set; }
     }
 }
