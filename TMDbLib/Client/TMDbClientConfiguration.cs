737a49dcc23b7ace23c1d06a73b0490b080606e1
Michael Bisbjerg <michael@mbwarez.dk>
2021-03-25 19:55:40 +0100

    Bad xmldocs

--- TMDbLib/Client/TMDbClientConfiguration.cs
+++ TMDbLib/Client/TMDbClientConfiguration.cs
@@ -49,17 +49,6 @@ namespace TMDbLib.Client
             return (await response.GetDataObject().ConfigureAwait(false));
         }
 
-        /// <summary>
-        /// FindAsync movies, people and tv shows by an external id.
-        /// The following trypes can be found based on the specified external id's
-        /// - Movies: Imdb
-        /// - People: Imdb, FreeBaseMid, FreeBaseId, TvRage
-        /// - TV Series: Imdb, FreeBaseMid, FreeBaseId, TvRage, TvDb
-        /// </summary>
-        /// <param name="source">The source the specified id belongs to</param>
-        /// <param name="id">The id of the object you wish to located</param>
-        /// <param name="cancellationToken">A cancellation token</param>
-        /// <returns>A list of all objects in TMDb that matched your id</returns>
         public async Task<Timezones> GetTimezonesAsync(CancellationToken cancellationToken = default(CancellationToken))
         {
             RestRequest req = _client.Create("timezones/list");
