810abe79bc40bc9c22ab1c2268550e31de1c4012
Danilo <danilobarretobezerra@gmail.com>
2021-06-14 15:56:44 -0300

    add translation data object

---
+++ TMDbLib/Objects/General/TranslationData.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace TMDbLib.Objects.General
+{
+    public class TranslationData
+    {
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("overview")]
+        public string Overview { get; set; }
+
+        [JsonProperty("tagline")]
+        public string Tagline { get; set; }
+    }
+}
