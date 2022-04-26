67d34a108dbc5d4925162686d08223e03927b742
TheUltimateCoder <no@e.mail>
2022-04-27 01:51:40 +0200

    - Added Watch Providers (https://developers.themoviedb.org/3/watch-providers) endpoints - Added tests for Watch Provider endpoints

---
+++ TMDbLibTests/ClientWatchProvidersTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+
+using TMDbLib.Objects.General;
+
+using TMDbLibTests.JsonHelpers;
+
+using Xunit;
+
+namespace TMDbLibTests
+{
+    public class ClientWatchProvidersTests : TestBase
+    {
+        [Fact]
+        public async Task TestGetRegions()
+        {
+            ResultContainer<WatchProviderRegion> watchProviderRegions = await TMDbClient.GetWatchProviderRegionsAsync();
+
+            await Verify(watchProviderRegions);
+        }
+
+        [Fact]
+        public async Task TestGetMovieWatchProviders()
+        {
+            ResultContainer<WatchProviderItem> watchProviders = await TMDbClient.GetMovieWatchProvidersAsync();
+
+            await Verify(watchProviders);
+        }
+
+        [Fact]
+        public async Task TestGetTvWatchProviders()
+        {
+            ResultContainer<WatchProviderItem> watchProviders = await TMDbClient.GetTvWatchProvidersAsync();
+
+            await Verify(watchProviders);
+        }
+    }
+}
