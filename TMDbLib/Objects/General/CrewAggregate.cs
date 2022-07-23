63d9dcd467d80e71c13c88d4b564ec327259f9d0
electroflame <electroflame@users.noreply.github.com>
2022-07-23 02:55:13 -0700

    Refactor Cast and Crew to use base classes.

--- TMDbLib/Objects/General/CrewAggregate.cs
+++ TMDbLib/Objects/General/CrewAggregate.cs
@@ -4,37 +4,10 @@ using TMDbLib.Objects.People;
 
 namespace TMDbLib.Objects.General
 {
-    public class CrewAggregate
+    public class CrewAggregate : CrewBase
     {
-        [JsonProperty("department")]
-        public string Department { get; set; }
-
-        [JsonProperty("id")]
-        public int Id { get; set; }
-
         [JsonProperty("jobs")]
         public List<CrewJob> Jobs { get; set; }
-
-        [JsonProperty("name")]
-        public string Name { get; set; }
-
-        [JsonProperty("profile_path")]
-        public string ProfilePath { get; set; }
-
-        [JsonProperty("gender")]
-        public PersonGender Gender { get; set; }
-
-        [JsonProperty("adult")]
-        public bool Adult { get; set; }
-
-        [JsonProperty("known_for_department")]
-        public string KnownForDepartment { get; set; }
-
-        [JsonProperty("original_name")]
-        public string OriginalName { get; set; }
-
-        [JsonProperty("popularity")]
-        public float Popularity { get; set; }
         
         [JsonProperty("total_episode_count")]
         public int TotalEpisodeCount { get; set; }
