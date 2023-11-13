5e49bf2f3bdc20d9771caef14a3e8b81d1c937d2
Daniel Martin Gonzalez <danimart1991@outlook.com>
2023-11-13 14:40:25 +0100

    Fix PR Comments

--- TMDbLib/Objects/Search/TvSeasonEpisode.cs
+++ TMDbLib/Objects/Search/TvSeasonEpisode.cs
@@ -33,7 +33,7 @@ namespace TMDbLib.Objects.Search
         public string ProductionCode { get; set; }
 
         [JsonProperty("runtime")]
-        public int Runtime { get; set; }
+        public int? Runtime { get; set; }
 
         [JsonProperty("season_number")]
         public int SeasonNumber { get; set; }
