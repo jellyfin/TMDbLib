150b43feaa5ae8785ea063520b05f12daa425585
keytrap-x86 <eugene.ball@outlook.fr>
2024-08-05 20:13:55 +0200

    Fix actors movies

--- TMDbLib/Objects/People/MovieRole.cs
+++ TMDbLib/Objects/People/MovieRole.cs
@@ -5,6 +5,10 @@ namespace TMDbLib.Objects.People
 {
     public class MovieRole
     {
+
+        [JsonProperty("order")]
+        public int Order { get; set; }
+
         [JsonProperty("adult")]
         public bool Adult { get; set; }
 
@@ -20,6 +24,12 @@ namespace TMDbLib.Objects.People
         [JsonProperty("original_title")]
         public string OriginalTitle { get; set; }
 
+        [JsonProperty("overview")]
+        public string Overview { get; set; }
+
+        [JsonProperty("original_language")]
+        public string OriginalLanguage { get; set; }
+
         [JsonProperty("poster_path")]
         public string PosterPath { get; set; }
 
@@ -28,5 +38,20 @@ namespace TMDbLib.Objects.People
 
         [JsonProperty("title")]
         public string Title { get; set; }
+
+        [JsonProperty("backdrop_path")]
+        public string BackdropPath { get; set; }
+
+        [JsonProperty("vote_count")]
+        public int VoteCount { get; set; }
+
+        [JsonProperty("vote_average")]
+        public double VoteAverage { get; set; }
+
+        [JsonProperty("popularity")]
+        public double Popularity { get; set; }
+
+        [JsonProperty("genre_ids")]
+        public int[] GenreIds { get; set; }
     }
 }
