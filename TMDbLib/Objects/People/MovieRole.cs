48fb1d8ba24bbb88b273d71a570c1c602bdcdcb8
keytrap-x86 <eugene.ball@outlook.fr>
2025-01-17 19:00:13 +0100

    Change properties order to match exactly the one on the documentation

--- TMDbLib/Objects/People/MovieRole.cs
+++ TMDbLib/Objects/People/MovieRole.cs
@@ -6,20 +6,20 @@ namespace TMDbLib.Objects.People
     public class MovieRole
     {
 
-        [JsonProperty("order")]
-        public int Order { get; set; }
-
         [JsonProperty("adult")]
-        public bool Adult { get; set; }
+        public bool Adult { get; set; } = true;
 
-        [JsonProperty("character")]
-        public string Character { get; set; }
+        [JsonProperty("backdrop_path")]
+        public string BackdropPath { get; set; }
 
-        [JsonProperty("credit_id")]
-        public string CreditId { get; set; }
+        [JsonProperty("genre_ids")]
+        public int[] GenreIds { get; set; }
 
         [JsonProperty("id")]
-        public int Id { get; set; }
+        public int Id { get; set; } = 0;
+
+        [JsonProperty("original_language")]
+        public string OriginalLanguage { get; set; }
 
         [JsonProperty("original_title")]
         public string OriginalTitle { get; set; }
@@ -27,34 +27,34 @@ namespace TMDbLib.Objects.People
         [JsonProperty("overview")]
         public string Overview { get; set; }
 
-        [JsonProperty("original_language")]
-        public string OriginalLanguage { get; set; }
+        [JsonProperty("popularity")]
+        public double Popularity { get; set; } = 0;
 
         [JsonProperty("poster_path")]
         public string PosterPath { get; set; }
 
         [JsonProperty("release_date")]
-        public DateTime? ReleaseDate { get; set; }
+        public string ReleaseDate { get; set; }
 
         [JsonProperty("title")]
         public string Title { get; set; }
 
-        [JsonProperty("backdrop_path")]
-        public string BackdropPath { get; set; }
-
-        [JsonProperty("vote_count")]
-        public int VoteCount { get; set; }
+        [JsonProperty("video")]
+        public bool Video { get; set; } = true;
 
         [JsonProperty("vote_average")]
-        public double VoteAverage { get; set; }
+        public double VoteAverage { get; set; } = 0;
 
-        [JsonProperty("popularity")]
-        public double Popularity { get; set; }
+        [JsonProperty("vote_count")]
+        public int VoteCount { get; set; } = 0;
 
-        [JsonProperty("genre_ids")]
-        public int[] GenreIds { get; set; }
+        [JsonProperty("character")]
+        public string Character { get; set; }
 
-        [JsonProperty("video")]
-        public bool Video { get; set; }
+        [JsonProperty("credit_id")]
+        public string CreditId { get; set; }
+
+        [JsonProperty("order")]
+        public int Order { get; set; } = 0;
     }
 }
